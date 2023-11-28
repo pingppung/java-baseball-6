b42ae335d25b30704f49a3471a90c2e6ad646cf1
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-28 16:12:20 +0900

    feat(InputView): 게임이 끝난 경우 재시작/종료를 구분하는 숫자 입력하는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,11 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_NUMBER_PROMPT = "숫자를 입력해주세요 : ";
+    private static final String RESTART_OR_END_PROMPT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public String getNumbers() {
         return inputWithPrompt(INPUT_NUMBER_PROMPT);
     }
-    
+
+    public String getRestartOrEnd() {
+        return inputWithPrompt(RESTART_OR_END_PROMPT);
+    }
+
     private String inputWithPrompt(String prompt) {
         System.out.println(prompt);
         return Console.readLine();
