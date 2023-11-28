14a09ed04dff513c52c5abaf5ebac42a514f6d7a
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-29 02:44:53 +0900

    feat(InputValidator): 입력값에 중복되는 값 존재하는지 확인하는 기능 구현

--- src/main/java/baseball/utils/InputValidator.java
+++ src/main/java/baseball/utils/InputValidator.java
@@ -1,12 +1,27 @@
 package baseball.utils;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputValidator {
     private static final String LENGTH_ERROR_MESSAGE = "3글자가 아닙니다.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "중복된 숫자가 있습니다.";
     private final static int NUMBER_SIZE = 3;
 
-    public void validateLength(String input) {
+    public void validateNumberLength(String input) {
         if (input.length() != NUMBER_SIZE) {
             throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
+        }
+    }
+
+    public void validateDuplicateNumber(String input) {
+        Set<Integer> numbers = new HashSet<>();
+        boolean hasDuplicate = input.chars()
+                .mapToObj(Character::getNumericValue)
+                .anyMatch(i -> !numbers.add(i));
+
+        if (hasDuplicate) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
         }
     }
 }
