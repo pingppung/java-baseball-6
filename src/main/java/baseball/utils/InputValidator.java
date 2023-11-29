492fb8773b5e55ae56105ed54334b22aad616868
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-30 02:34:27 +0900

    feat(InputValidator): 사용자의 재시작/종료를 구분하는 입력값에 대한 유효성 검사 구현

--- src/main/java/baseball/utils/InputValidator.java
+++ src/main/java/baseball/utils/InputValidator.java
@@ -8,8 +8,11 @@ public class InputValidator {
     private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 숫자가 있습니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 1~9 범위를 벗어난 숫자가 있습니다.";
     private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자 입력만 가능합니다.";
+    private static final String GAME_DECISION_ERROR_MESSAGE = "[ERROR] 재시작 : 1 or 종료 : 2만 입력이 가능합니다.";
     private static final int RANDOM_MIN_NUMBER = 1;
     private static final int RANDOM_MAX_NUMBER = 9;
+    private static final int RESTART_NUMBER = 1;
+    private static final int END_NUMBER = 2;
     private static final int NUMBER_SIZE = 3;
 
     public static void validateNumberLength(String input) {
@@ -43,6 +46,12 @@ public class InputValidator {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
+        }
+    }
+
+    public static void validateRestartOrEndNumber(int input) {
+        if (input != RESTART_NUMBER && input != END_NUMBER) {
+            throw new IllegalArgumentException(GAME_DECISION_ERROR_MESSAGE);
         }
     }
 }
