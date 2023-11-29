3338296d02033c329fcc781b5fbe4eb7264a3e9d
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-30 02:04:20 +0900

    feat(Player): 사용자의 서로 다른 3자리 수 입력값을 리스트로 변환하는 기능 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,13 +1,15 @@
 package baseball.domain;
 
 import baseball.utils.InputValidator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public Player(String input) {
         validate(input);
+        this.numbers = convertInputToNumbers(input);
     }
 
     private void validate(String input) {
@@ -15,5 +17,14 @@ public class Player {
         InputValidator.validateNumberLength(input);
         InputValidator.validateDuplicateNumber(input);
         InputValidator.validateNumberInRange(input);
+    }
+
+    private List<Integer> convertInputToNumbers(String input) {
+        List<Integer> convertedNumbers = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            int number = Character.getNumericValue(c);
+            convertedNumbers.add(number);
+        }
+        return convertedNumbers;
     }
 }
