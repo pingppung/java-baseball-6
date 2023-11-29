21f761248f89deffc271c6d979dc43248993b926
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-29 16:12:56 +0900

    feat(InputValidator): 숫자가 아닌 입력값을 검증하는 유효성 검사 기능 구현

--- src/main/java/baseball/utils/InputValidator.java
+++ src/main/java/baseball/utils/InputValidator.java
@@ -7,6 +7,7 @@ public class InputValidator {
     private static final String LENGTH_ERROR_MESSAGE = "[ERROR] 3자리의 수가 아닙니다.";
     private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복된 숫자가 있습니다.";
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 1~9 범위를 벗어난 숫자가 있습니다.";
+    private static final String NON_NUMERIC_ERROR_MESSAGE = "[ERROR] 숫자 입력만 가능합니다.";
     private static final int RANDOM_MIN_NUMBER = 1;
     private static final int RANDOM_MAX_NUMBER = 9;
     private final static int NUMBER_SIZE = 3;
@@ -34,6 +35,14 @@ public class InputValidator {
             if (digit < RANDOM_MIN_NUMBER || digit > RANDOM_MAX_NUMBER) {
                 throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
             }
+        }
+    }
+
+    public void validateNonNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NON_NUMERIC_ERROR_MESSAGE);
         }
     }
 }
