191d6bc215fbb9e6ea4cdccf5f023899a57333e5
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-11-30 19:36:53 +0900

    test(InputValidatorTest): 게임 재시작/종료에 대한 입력값 예외처리 테스트

--- src/test/java/baseball/utils/InputValidatorTest.java
+++ src/test/java/baseball/utils/InputValidatorTest.java
@@ -1,80 +1,102 @@
 package baseball.utils;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
+import baseball.constants.BaseballConstants;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class InputValidatorTest {
-    private InputValidator inputValidator;
 
-    @BeforeEach
-    void setUp() {
-        inputValidator = new InputValidator();
-    }
 
-    @DisplayName("입력값이_3자리일_경우")
+    @DisplayName("입력값이_지정_길이일_경우")
     @Test
     void validateInputNumberLength() {
         String input = "123";
-        inputValidator.validateNumberLength(input);
+        InputValidator.validateNumberLength(input);
     }
 
-    @DisplayName("입력값이_3자리가_아닌_경우_예외처리")
+    @DisplayName("입력값이_지정_길이가_아닌_경우_예외처리")
     @Test
     void invalidateInputNumberLength() {
         String input = "12345";
-        assertThatThrownBy(() -> inputValidator.validateNumberLength(input))
+        int pitchCount = BaseballConstants.PITCHES.value;
+        assertThatThrownBy(() -> InputValidator.validateNumberLength(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 3자리의 수가 아닙니다.");
+                .hasMessage(ErrorMessages.LENGTH_ERROR.formatMessage(pitchCount));
     }
 
     @DisplayName("입력값에_중복된_숫자가_존재하지_않는_경우")
     @Test
     void validateDuplicateNumber() {
         String input = "123";
-        inputValidator.validateDuplicateNumber(input);
+        InputValidator.validateDuplicateNumber(input);
     }
 
     @DisplayName("입력값에_중복된_숫자가_존재할_경우")
     @Test
     void invalidateDuplicateNumber() {
         String input = "112";
-        assertThatThrownBy(() -> inputValidator.validateDuplicateNumber(input))
+        assertThatThrownBy(() -> InputValidator.validateDuplicateNumber(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 중복된 숫자가 있습니다.");
+                .hasMessage(ErrorMessages.DUPLICATE_ERROR.message);
     }
 
     @DisplayName("입력값이_유효한_범위내에_존재하는_경우")
     @Test
     void validateNumberInRange() {
         String input = "137";
-        inputValidator.validateNumberInRange(input);
+        InputValidator.validateNumberInRange(input);
     }
 
     @DisplayName("입력값의_숫자가_범위를 벗어나는_경우")
     @Test
     void invalidateNumberInRange() {
         String input = "109";
-        assertThatThrownBy(() -> inputValidator.validateNumberInRange(input))
+        int randomMax = BaseballConstants.RANDOM_MAX_NUMBER.value;
+        int randomMin = BaseballConstants.RANDOM_MIN_NUMBER.value;
+        assertThatThrownBy(() -> InputValidator.validateNumberInRange(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1~9 범위를 벗어난 숫자가 있습니다.");
+                .hasMessage(ErrorMessages.RANGE_ERROR.formatMessage(randomMin, randomMax));
     }
 
     @DisplayName("입력값에_문자없이_숫자로만_이루어진_경우")
     @Test
     void validateNonNumeric() {
         String input = "123";
-        inputValidator.validateNonNumeric(input);
+        InputValidator.validateNonNumeric(input);
     }
 
     @DisplayName("입력값에_문자가_존재하는_경우")
     @Test
     void invalidateNonNumeric() {
         String input = "1S2";
-        assertThatThrownBy(() -> inputValidator.validateNonNumeric(input))
+        assertThatThrownBy(() -> InputValidator.validateNonNumeric(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 숫자 입력만 가능합니다.");
+                .hasMessage(ErrorMessages.NON_NUMERIC_ERROR.message);
+    }
+
+    @DisplayName("게임_재시작_또는_종료_확인")
+    @ParameterizedTest
+    @CsvSource({
+            "1, true",
+            "2, false"
+    })
+    void validateRestartOrEndGame(int number, boolean expectedResult) {
+        assertEquals(expectedResult, InputValidator.validateRestartOrEndNumber(number));
+    }
+
+    @DisplayName("게임_재시작_또는_종료값이_아닌_다른_숫자_입력_예외처리")
+    @Test
+    void invalidateRestartOrEndGame() {
+        int input = 3;
+        int restart = BaseballConstants.RESTART_NUMBER.value;
+        int end = BaseballConstants.END_NUMBER.value;
+        assertThatThrownBy(() -> InputValidator.validateRestartOrEndNumber(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessages.GAME_DECISION_ERROR.formatMessage(restart, end));
     }
 }
